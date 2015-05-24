96a6d983841ee4318b95087bf85548f51f487322
armorik83 <okunokentaro@me.com>
2015-05-24 15:05:07 +0900

    chore(gulpfile): add task "build"

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,5 @@ gulp.task('default', ['less'], function() {
     .watch(['styles/*.less'], ['less'])
     .on('error', function(err) { process.exit(1); });
 });
+
+gulp.task('build', shell.task(['browserify -e ./app/src/index.js -o ./app/bundle.js -t [ babelify --optional es7.decorators ]']));
